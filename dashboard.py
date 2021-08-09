d393a8c482b06338a3d777a756b4bc2f44e66d6f
Durieux <durieux.arnaud@hotmail.com>
2021-08-09 15:54:02 +0200

    bit of code cleaning + started dashboard

---
+++ dashboard.py
@@ -0,0 +1,49 @@
+from processing import initialize_dataframe
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+@st.cache
+def load_data():
+    return initialize_dataframe()
+
+
+def plot_pie(column_name, dataframe):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle(column_name)
+
+    data = dataframe.groupby(column_name).count().iloc[:, 0]
+    data_Attrited = (
+        dataframe[dataframe["Attrition_Flag"] == "Attrited Customer"].groupby(column_name).count().iloc[:, 0]
+    )
+    data.name = column_name
+    labels = data.index
+    labels_Attrited = data_Attrited.index
+    ax1.title.set_text("All")
+    ax2.title.set_text("Attrited Customer")
+    ax1.pie(data.T, autopct="%.1f%%", startangle=90, labels=labels)
+    ax2.pie(data_Attrited.T, autopct="%.1f%%", startangle=90, labels=labels_Attrited)
+
+    # plt.show()
+    return fig
+
+
+df = df = pd.read_csv("assets/BankChurners.csv")
+
+df = df.drop(
+    "Naive_Bayes_Classifier_Attrition_Flag_Card_Category"
+    "_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1",
+    axis=1,
+)
+df = df.drop(
+    "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_"
+    "Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2",
+    axis=1,
+)
+df = df.drop("CLIENTNUM", axis=1)
+
+st.title("Food Demand Forecasting — Analytics Vidhya")
+st.pyplot(plot_pie("Gender", df))
+st.pyplot(plot_pie("Card_Category", df))
--- model.py
+++ model.py
@@ -1,16 +1,35 @@
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import pandas as pd
+from imblearn.over_sampling import ADASYN, SMOTE
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.metrics import classification_report, cohen_kappa_score, confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from processing import initialize_dataframe
-from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+
+from processing import initialize_dataframe
 
 df = initialize_dataframe()
 
 
-models = [RandomForestClassifier(n_estimators=100), KNeighborsClassifier(n_neighbors=3)]
-
-# model = RandomForestClassifier(n_estimators=100)
+models = [
+    RandomForestClassifier(n_estimators=100),
+    KNeighborsClassifier(n_neighbors=3),
+    DecisionTreeClassifier(max_depth=14),
+    GradientBoostingClassifier(n_estimators=1500, learning_rate=1, max_features=10, max_depth=2, random_state=0),
+    XGBClassifier(colsample_bytree=0.9, learning_rate=0.2, max_depth=7, label_encoder=False),
+    lgb.LGBMClassifier(
+        max_depth=-1,
+        random_state=42,
+        silent=True,
+        metric="None",
+        n_jobs=5,
+        n_estimators=1000,
+    ),
+    ExtraTreesClassifier(),
+]
 
 X = df.drop("has_left", axis=1)
 y = df["has_left"]
@@ -18,19 +37,87 @@ y = df["has_left"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-def run_model(model):
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+class Data:
+    pass
+
+
+data = Data()
+data.X_train = X_train
+data.X_test = X_test
+data.y_train = y_train
+data.y_test = y_test
+
+sm = SMOTE(random_state=42)
+X_smote, y_smote = sm.fit_resample(X, y)
+X_train_smote, X_test_smote, y_train_smote, y_test_smote = train_test_split(
+    X_smote, y_smote, test_size=0.2, random_state=42
+)
+
+data_smote = Data()
+data_smote.X_train = X_train_smote
+data_smote.X_test = X_test_smote
+data_smote.y_train = y_train_smote
+data_smote.y_test = y_test_smote
+
+ada = ADASYN(random_state=42)
+X_adasyn, y_adasyn = ada.fit_resample(X, y)
+X_train_adasyn, X_test_adasyn, y_train_adasyn, y_test_adasyn = train_test_split(
+    X_adasyn, y_adasyn, test_size=0.2, random_state=42
+)
+
+data_adasyn = Data()
+data_adasyn.X_train = X_train_adasyn
+data_adasyn.X_test = X_test_adasyn
+data_adasyn.y_train = y_train_adasyn
+data_adasyn.y_test = y_test_adasyn
+
+
+def plot_feature_importance():
+
+    forest = RandomForestClassifier(random_state=0)
+    forest.fit(X_train, y_train)
+
+    importances = forest.feature_importances_
+    feature_names = [f"feature {column}" for column in X.columns]
+
+    forest_importances = pd.Series(importances, index=feature_names)
+    forest_importances.nlargest(10).sort_values().plot(kind="barh")
+    plt.title("Top 10 Important Features")
+    plt.show()
+
+
+def run_model(model, data):
+
+    model.fit(data.X_train, data.y_train)
+    y_pred = model.predict(data.X_test)
 
     print("Classification Report: ")
-    print(classification_report(y_test, y_pred))
-
-    print(confusion_matrix(y_test, y_pred))
-    print(confusion_matrix(y_train, model.predict(X_train)))
-    print(confusion_matrix(y, model.predict(X)))
+    print(classification_report(data.y_test, y_pred))
+    print("confusion matrix test")
+    print(confusion_matrix(data.y_test, y_pred))
+    print("confusion matrix train")
+    print(confusion_matrix(data.y_train, model.predict(data.X_train)))
+    print("Cohen Kappa score")
+    print(cohen_kappa_score(data.y_test, y_pred))
 
 
 for model in models:
     print("------------------------------------")
     print("model = " + str(model))
-    run_model(model)
+    run_model(model, data)
+
+print("------------------------------------")
+print("SMOTE")
+
+for model in models:
+    print("------------------------------------")
+    print("model = " + str(model))
+    run_model(model, data_smote)
+
+print("------------------------------------")
+print("ADASYN")
+
+for model in models:
+    print("------------------------------------")
+    print("model = " + str(model))
+    run_model(model, data_adasyn)
